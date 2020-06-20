407a489679242ff7bba4ea0a53d2c0820cf085ff
fei <nilqaq@gmail.com>
2020-06-21 00:12:53 +0800

    init router

--- router/admin/router.go
+++ router/admin/router.go
@@ -1,10 +1,18 @@
 package admin
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
+	"github.com/kataras/iris/v12/core/router"
+)
 
 /**
 Admin 路由
 */
 func InitRouter(app *iris.Application) {
-
+	app.PartyFunc("/admin", func(p router.Party) {
+		p.Get("/", func(ctx context.Context) {
+			ctx.JSON(map[string]string{"hello": "1231231"})
+		})
+	})
 }
